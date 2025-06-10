3adea1154aa6c622a122bbc20cf1ad4ec42b77bb
BiyangaKalupahana <biyangadilmini1@gmail.com>
2025-06-10 20:35:40 +0530

    9

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3'>
+
+        <div>
+
+        </div>
+        <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright <FontAwesomeIcon icon="fa-solid fa-copyright" />2025 QuickBlog - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
