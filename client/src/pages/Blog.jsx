3640c712204100fabae11c50bce69c4c98658e45
BiyangaKalupahana <biyangadilmini1@gmail.com>
2025-06-10 22:40:34 +0530

    11

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { blog_data } from '../assets/assets'
 
 const Blog = () => {
-  return (
+
+  const {id} = useParams()
+
+  const [data, setData] = useState(null)
+
+  const fetchBlogData=  async () =>{
+  const data = blog_data.find(item => item._id === id)
+  setData(data)
+  }
+
+  useEffect(()=>{
+    fetchBlogData()
+  },[])
+
+  return data ? (
     <div>
-        <h1>Blog</h1>
+        <div></div>
+
+        <div></div>
     </div>
-  )
+  ): <div>Loading...</div>
 }
 
 export default Blog
