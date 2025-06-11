4ccc6e6b384e0a026bf64a7d72bf9d5c4fbc690b
BiyangaKalupahana <biyangadilmini1@gmail.com>
2025-06-11 15:42:38 +0530

    13

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
-        
+    <div className='fle-1 p-4 md:p-10 bg-blue-50/50'>
+       <div className='flex flex-wrap gap-4'>
+        <div>
+          <img src={assets.dashboard_icon_1} alt="" />
+          <div>
+            <p>{dashboard}</p>
+          </div>
+        </div>
+        </div> 
     </div>
   )
 }
