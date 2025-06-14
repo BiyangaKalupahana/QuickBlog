10a84218bcc727ddf43878eb1ac2a3b0ff0e732b
BiyangaKalupahana <biyangadilmini1@gmail.com>
2025-06-14 11:10:44 +0530

    22

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,30 +1,31 @@
+import Blog from '../models/Blog';
+
 // controllers/adminController.js
 const Admin = require('../models/adminModel'); // adjust path as needed
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const loginAdmin = async (req, res) => {
-  const { email, password } = req.body;
+export const adminLogin = async (requestAnimationFrame, res) =>{
+  try{
+    const {email, password} = requestAnimationFrame.body;
 
-  try {
-    const admin = await Admin.findOne({ email });
-    if (!admin) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+    if(email !==process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD){
+      return res.json({success: false, message: "Invalid Credentials"})
     }
 
-    const isMatch = await bcrypt.compare(password, admin.password);
-    if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
+    const token = jwt.sign({email}, process.env.JWT_SECRET)
+    res.json({success: true, token})
 
-    const token = jwt.sign({ id: admin._id }, 'your_jwt_secret', {
-      expiresIn: '1h'
-    });
-
-    res.json({ token });
-  } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+  } catch (error){
+    res.json({success: false, message: error.message})
   }
-};
+}
 
-module.exports = { loginAdmin };
+export const getAllBlogsAdmin = async (req, res) =>{
+  try{
+    const blogs = await Blog.find({}).sort({createdAt: -1});
+    res.json({success: true, blogs})
+  } catch (error){
+      res.json({success: false, message: error.message})
+  }
+} 
